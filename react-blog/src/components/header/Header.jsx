aa547da4f0fc06c16d0273f093ed8c54cea463be
aniktonu <aniktonu@gmail.com>
2022-12-26 17:57:43 +0600

    NavLink activate with red color done

--- react-blog/src/components/header/Header.jsx
+++ react-blog/src/components/header/Header.jsx
@@ -1,20 +1,38 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './Header.css'
+
 
 
 
 const Header = () => {
+
+    let activeStyle = {
+        textDecoration: "underline",
+        color: "red"
+    };
+
+
+
+
     return (
         <>
+
+
             <Navbar bg="primary" variant="dark">
                 <Container>
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="my-auto my-auto-inner">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    <Nav className="my-auto my-auto-inner"
+                    >
+
+                        <Nav.Link as={NavLink} style={({ isActive }) => isActive ? activeStyle : undefined
+                        } to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} style={({ isActive }) => isActive ? activeStyle : undefined
+                        } to="/about">About</Nav.Link>
+                        <Nav.Link as={NavLink} style={({ isActive }) => isActive ? activeStyle : undefined
+                        } to="/contact">Contact</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
